cfd3e72e30be16aa2b3b8d256140dd4251578b8f
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-04 10:36:25 +0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,14 @@
-<?php
+<?php 
 
-  $link = mysqli_connect("localhost", "root" , "", "e-bookcart");
+
+  $host = "remotemysql.com";
+  $db = "6JBOpMPTpi" ; 
+  $user = "6JBOpMPTpi";
+  $pass = "za8u35Pbzw";
+  //$charset =
+
+  //$link = mysqli_connect("localhost", "root" , "", "e-bookcart");
+
+  $link = mysqli_connect($host, $user , $pass , $db);
 
 ?>
