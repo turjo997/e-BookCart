b028ed7febaf7d5fda69abb53a16a99822830d40
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-21 21:40:23 +0600

    Update index.php

--- index.php
+++ index.php
@@ -13,9 +13,32 @@
       <!---bootstrapt cdn--->
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
 			
-				<style>
+				  <style>
+
+                header{
+
+                  background: url("coverimages/projectcover1.jpg");
+                  background-position: center;
+                  background-attachment: fixed;
+                  background-repeat: no-repeat;
+                  background-size: cover;
+            }
+                 .banner-container{
+
+                height: 100vh;
+                }
+
+                .banner-container h1{
+
+                font-size: 3.2rem;
+                background-color: rgb((0,0,128));
+                padding: 10px 20px;
+             
+                border-radius: 5px;
+
+                }
 	
-	            </style>
+	      </style>
 			<meta charset = "UTF-8">
             <meta name="viewport" content="width = device-width , initial-scale = 1.0">
             <meta http-equiv="X-UA-compatible" >
@@ -28,31 +51,37 @@
 
      <div class="container-fluid">
 
+     <header>
 
-     <form method = "post" action="" >
-        <input type="submit" name="submit" class= "btn btn-secondary" value="submit">
-                   
-                                  
-         <?php
 
-          if(isset($_POST['submit'])){
-            header('location: admin/login.php');
-           }
-         //equire_once 'admin';
-         ?>
+
+     <div class="banner-container d-flex justify-content-center align-items-center">
+
+      <div class="banner-contents text-center">
+
+        <h1 class="font-weight-bolder text-danger mb-5 ">Want to visit the website ? Click the user panel</h1>
+
+      
+          <a class = "btn btn-lg btn-danger mr-3" href="admin/login.php"></i>Admin Pamel</a>
+          
+          <a class = "btn btn-lg btn-danger mr-3" href="user/main.php"></i>User Panel</a>
+
+      </div>
+
+
+      </div>
+
+
+    
+
+       </header>
      
      </div>
                  
 
-     </div>       
+  
 
-     
-
-			
-
-
-
-
+    
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
