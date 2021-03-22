8ec676a8c7f944fe06eff91f1c6b065b8f71e0f5
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-22 17:35:33 +0600

    Update main.php

--- user/main.php
+++ user/main.php
@@ -3,4 +3,50 @@
  require_once 'header.php'
 
 ?>
+
+<div class="container-fluid">
+
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+ 
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+   
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php"><b>Contact</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="aboutus.php"><b>About Us</b></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <b> Categories </b>
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#"><b>Best Sellers</b></a>
+          <a class="dropdown-item" href="#">Novels</a>
+          <a class="dropdown-item" href="#">Actions & Adventures</a>
+          <a class="dropdown-item" href="#">Comics</a>
+          <a class="dropdown-item" href="#">Detactive</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+           
+</div>       
+
+
+
+
+
+
+<br><br><br><br><br><br>
+<?php 
+         
+ require_once 'footer.php'
+
+?>
+
+
     
