c9fcf8cca7556b38fa92f8222e39a6dcf8f3c17c
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-26 00:11:49 +0600

    Add files via upload

---
+++ user/aboutus.php
@@ -0,0 +1,44 @@
+<?php
+   require_once 'header.php';
+?>
+
+<div class="container">
+
+<div class="row">
+
+        
+        <div class="col-lg-5">
+        <br><br><br>
+        <img style="width:500px; height:100%" class = "d-block m-auto img-fluid"src="images/bookLogo.png" alt="">
+
+        </div>
+        <div class="col-lg-1"></div>
+
+        <div class="col-lg-6">
+        <br><br><br>
+        <h1 style="color:#0f6ba4"class="text-center">About E-BookCart</h1>
+
+        <p style="color:#ca4286;font-size:23px;" class="text-center font-weight: bolder">E-BookCart is an e-commerce based website.
+         It is a platform where the user can order books according to their choices available on the website. An user 
+         as well as a non user , can give their important feedback to the adminstrator. After login , user can see their order list added to the cart before , change their password , watch their profile.   
+         Adminstrator have some flexibilities also . Admin can add books with proper information , remove books which is less sell out and very importantly take the users order.         
+        </p>
+
+
+
+        </div>
+
+
+
+
+</div>
+
+
+
+
+</div>
+
+
+<?php
+   require_once 'footer.php';
+?>  
