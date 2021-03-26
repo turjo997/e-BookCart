d6da9d9013ca4ad36872319fab37e02af85e2d48
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-26 18:27:13 +0600

    Update aboutus.php

--- user/aboutus.php
+++ user/aboutus.php
@@ -1,8 +1,73 @@
 <?php
-   require_once 'header.php';
+//   require_once 'header.php';
 ?>
 
-<div class="container">
+
+<!DOCTYPE html>
+
+<html lang="en">
+     <head>
+	 
+	        <title>User</title>
+          <link rel = "shortcut icon" type="image" href="images/bookLogo.png"/>	
+			
+			<link rel = "stylesheet" href="userStyle.css"/>	
+
+            
+     <!--font awsome-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous"/>
+
+     <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:wght@100&family=Roboto:wght@100&display=swap" rel="stylesheet">
+      <!---bootstrapt cdn--->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
+
+ 
+
+				<style>
+	
+	            </style>
+			      <meta charset = "UTF-8">
+            <meta name="viewport" content="width = device-width , initial-scale = 1.0">
+            <meta http-equiv="X-UA-compatible" >
+
+
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+       
+
+	 </head>
+   
+	 
+	 <body>
+
+  
+
+
+  <div class="container-fluid">
+
+     <nav class="navbar navbar-expand-md">
+                   
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                     <span><i class="fas fa-bars navbar-icon"></i></span>
+                   </button>
+
+                   <a class="navbar-brand" href="">
+                         <img src="images/bookLogo.png" alt="logo">
+                  
+                   </a>
+
+
+                   <div class="collapse navbar-collapse"  id="navbarNavDropdown">
+                     <ul class="navbar-nav ml-auto">
+
+                     </ul>
+                   </div>
+                 </nav>
+                
+    </div>       
+
+
+
+    <div class="container">
 
 <div class="row">
 
@@ -39,6 +104,23 @@
 </div>
 
 
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+			
+
+	 </body>
+
+
+
+</html> 
+
+
+
+
+
 <?php
-   require_once 'footer.php';
-?>  
+     require_once 'footer.php';
+?>
+
