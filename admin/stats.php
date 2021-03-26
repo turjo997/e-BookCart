e3393d2eebb6dfda231651865a744be84da7099f
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-26 21:34:53 +0600

    Add files via upload

---
+++ admin/stats.php
@@ -0,0 +1,164 @@
+<?php
+  require_once '../config.php';
+  $query ="SELECT category , count(*) as number  from books group by category";
+  $result = mysqli_query($link, $query); 
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+     <head>
+	 
+	        <title>admin</title>
+          <link rel = "shortcut icon" type="image" href="images/bookLogo.png"/>	
+
+
+          <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>  
+           <script type="text/javascript">  
+           google.charts.load('current', {'packages':['corechart']});  
+           google.charts.setOnLoadCallback(drawChart);  
+           function drawChart()  
+           {  
+                var data = google.visualization.arrayToDataTable([  
+                          ['Category', 'Number'],  
+                          <?php  
+                          while($row = mysqli_fetch_array($result))  
+                          {  
+                               echo "['".$row["category"]."', ".$row["number"]."],";  
+                          }  
+                          ?>  
+                     ]);  
+                var options = {  
+                      title: 'Statistics of selling books',  
+                      //is3D:true,  
+                      pieHole: 0.4  
+                     };  
+                var chart = new google.visualization.PieChart(document.getElementById('piechart'));  
+                chart.draw(data, options);  
+           }  
+           </script>  
+          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-ajaxy/1.6.1/scripts/jquery.ajaxy.min.js" integrity="sha512-bztGAvCE/3+a1Oh0gUro7BHukf6v7zpzrAb3ReWAVrt+bVNNphcl2tDTKCBr5zk7iEDmQ2Bv401fX3jeVXGIcA==" crossorigin="anonymous"></script>
+
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.6.0/Chart.js"></script>
+         
+
+
+
+		    	<link rel = "stylesheet" href="style.css"/>	
+
+
+
+            
+     <!--font awsome-->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous"/>
+
+     <link href="https://fonts.googleapis.com/css2?family=Handlee&family=Montserrat:wght@100&family=Roboto:wght@100&display=swap" rel="stylesheet">
+      <!---bootstrapt cdn--->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">	
+			
+				<style>
+	
+	      </style>
+			      <meta charset = "UTF-8">
+            <meta name="viewport" content="width = device-width , initial-scale = 1.0">
+            <meta http-equiv="X-UA-compatible" >
+
+           
+  
+	 </head>
+	
+	  
+	 
+	 <body>
+
+     <div class="container-fluid">
+     <nav class="navbar navbar-expand-md">
+                   
+                   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                     <span><i class="fas fa-bars navbar-icon"></i></span>
+                   </button>
+
+                   <a class="navbar-brand" href="profile.php">
+                         <img src="images/bookLogo.png" alt="logo">
+                         <!--https://i.pinimg.com/originals/2a/48/ca/2a48cae52a31e76376d82ba4bb32824a.png-->
+                   </a>
+
+
+                   <div class="collapse navbar-collapse"  id="navbarNavDropdown">
+                     <ul class="navbar-nav ml-auto">
+
+                  
+                           
+                     </ul>
+                   </div>
+                 </nav>
+     </div>    
+
+
+
+     <br /><br />  
+           <div style="width:900px;">  
+                <h3 style="color:#ca4286;font-size:27px;" class="text-center font-weight: bolder">Pie Chart over selling books</h3>  
+                <br />  
+                <div id="piechart" style="width: 900px; height: 500px;"></div>  
+           </div>  
+
+
+       
+
+</div>
+
+     
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+			
+		
+	 </body>
+
+
+
+</html> 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+  require_once 'footer.php';
+?>
