1a7d96b483ca79aa48a9d4c34dd194ea05099ebf
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-03-26 18:48:22 +0600

    Update orders.php

--- user/orders.php
+++ user/orders.php
@@ -49,7 +49,7 @@ require_once '../config.php';
                         <tr>
                             <td><?php echo $value["item_name"]; ?></td>
                             <td><?php echo $value["item_quantity"]; ?></td>
-                            <td>$ <?php echo $value["product_price"]; ?></td>
+                            <td><?php echo $value["product_price"]; ?></td>
                                 
                                  <?php
                             $book_name = $value["item_name"];
