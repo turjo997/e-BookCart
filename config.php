83ce346a6e463d0abb4fa6a6eb6f2e988bf6a2d8
Ullash Bhattacharjee(Turjo) <53423899+turjo997@users.noreply.github.com>
2021-07-22 20:01:03 +0600

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,21 @@
   $user = "6JBOpMPTpi";
   $pass = "za8u35Pbzw";
   $charset = "utf8mb4";
-*/
+
   $host = "remotemysql.com";
   $db = "9Ktv1ogX2r" ; 
   $user = "9Ktv1ogX2r";
   $pass = "8f00cEm3up";
   $charset = "utf8mb4";
+*/
+
+
+ $host = "remotemysql.com";
+  $db = "fTEYlQ5O2P" ; 
+  $user = "fTEYlQ5O2P";
+  $pass = "8f00cEm3up";
+  $charset = "utf8mb4";
+
 
   //$link = mysqli_connect("localhost", "root" , "", "e-bookcart");
 
